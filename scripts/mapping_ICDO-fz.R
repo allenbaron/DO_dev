84039d0a3b3105dd5f25ef7b53e44abcd6fca4ab
J. Allen Baron <jabaron.phd@gmail.com>
2022-02-09 15:04:04 -0500

    Update for DO v ICD-O fz matching using all terms
    Results are the best match in DO for each ICD-O label.
    
    Updates:
    - Includes all labels from ICD-O (preferred, synonyms, related)
    and most from DO (preferred & exact synonyms)
    - Calculates maximum distance for changes using all labels from
    BOTH datasets (= 34)
    - Outputs matches for review

--- scripts/mapping_ICDO-fz.R
+++ scripts/mapping_ICDO-fz.R
@@ -28,56 +28,98 @@ standardize_label <- function(term) {
 }
 
 
-# Files, input ------------------------------------------------------------
+# File paths --------------------------------------------------------------
 
+# Input
 icdo_xl_file <- here::here("data/mapping/2021_ICDO_update_preferred terms.xlsx")
 do_owl_file <- here::here("../Ontologies/HumanDiseaseOntology/src/ontology/doid.owl")
-sparql_cell_prolif <- here::here("sparql/DO-cell_prolif-id_label.rq")
+sparql_cp <- here::here("sparql/DO-cell_prolif-id_label_exsyn.rq")
+
+# Output
+match_res_file <- here::here("data/mapping/do_icdo-fz_match.csv")
 
 
-# Initiate test to estimate time required for matching --------------------
-#   1. limit ICD-O to leukemia (5 terms only)
-#   2. limit DO to cancer branch (all terms)
 
-# Read ICD-O, leukemia terms (NOS previously removed)
+# Load & standardize data  ------------------------------------------------
+
+# Read all ICD-O terms, including synonyms
 icdo_data <- readxl::read_excel(
     icdo_xl_file,
-    sheet = "leukemia",
+    sheet = "all terms",
     col_types = "text"
-)
+) %>%
+    ##### tidy
+    # column names
+    dplyr::rename(icdo_id = `ICDO3.2`, type = Level, icdo_label = Term) %>%
+    # labels
+    dplyr::mutate(
+        # remove NOS (only found in ICD-O)
+        term_no_nos = stringr::str_remove(icdo_label, "[, ]*NOS"),
+        term_std = standardize_label(term_no_nos),
+    # type - to match DO cellular prolif data
+        type = stringr::str_to_lower(type)
+    )
 
-# Extract DO cancer terms (release: v2022-01-31)
+
+# Extract DO cancer terms & exact synonyms (release: v2022-01-31)
 do_owl <- py_rdf$read(do_owl_file)
-do_cp <- py_rdf$sparql_query(do_owl, query = sparql_cell_prolif) %>%
-    tibble::as_tibble()
+do_cp <- py_rdf$sparql_query(do_owl, query = sparql_cp) %>%
+    tibble::as_tibble() %>%
+    ##### tidy
+    # column names
+    dplyr::rename(doid = id, do_label = label) %>%
+    dplyr::mutate(
+    # labels
+        term_std = standardize_label(do_label),
+    # type - to match ICD-O data
+        type = stringr::str_remove(type, "exact_")
+    )
 
 
-# Standardize terms in both resources (see custom function definition above
-#   for details)
-icdo_data <- icdo_data %>%
-    dplyr::mutate(term_std = standardize_label(Term))
-do_cp <- do_cp %>%
-    dplyr::mutate(term_std = standardize_label(label))
+# Complete fuzzy string matching ------------------------------------------
 
-# Calculate average string length to set maximum distance parameter,
-#   NOTE: There are MANY more terms in DO, so I'll just check DO and use the
-#       3rd quartile.
-est_max_dist <- purrr::map_int(do_cp$term_std, stringr::str_length) %>%
+# Set maximum allowable string match distance to 3rd quartile of string length
+#   across both resources.
+max_dist_3q <- c(
+    purrr::map_int(do_cp$term_std, stringr::str_length),
+    purrr::map_int(icdo_data$term_std, stringr::str_length)
+) %>%
     summary() %>%
     .[["3rd Qu."]]
 
 
-# Test Fuzzy String Matching with 5 ICD-O leukemia terms to 2,736 DO cancer
-#   terms
-time5 <- system.time(
-    icdo5_do <- DO.utils::match_fz(
-        icdo_data$term_std[1:5],
-        do_cp$term_std,
-        maxDist = est_max_dist
+# match ICD-O to DO, since there are more DO terms and the extra matches that
+#   will result from the reverse comparison are undesirable (will only make for
+#   more work).
+system.time(
+    icdo_do <- DO.utils::match_fz(
+        # ignore label duplicates to speed up matching
+        unique(icdo_data$term_std),
+        unique(do_cp$term_std),
+        maxDist = max_dist_3q
     )
-)
+)   # takes ~ 22s
 
-######### RESULT: test took only 0.011 seconds ###############
-# Full ICD-O set should take ~ 2.5s (1,143 / 5 * 0.011s)
-time5
-icdo5_do
+
+# Tidy and save data ------------------------------------------------------
+
+tidy_df <- icdo_do %>%
+    dplyr::left_join(
+        dplyr::select(icdo_data, -term_no_nos),
+        by = c("x" = "term_std")
+    ) %>%
+    dplyr::rename(icdo_std = x, icdo_type = type) %>%
+    dplyr::left_join(do_cp, by = c("table_match" = "term_std")) %>%
+    dplyr::rename(do_std = table_match, do_type = type) %>%
+    dplyr::select(
+        # icdo info
+        icdo_id, icdo_type, icdo_label,
+        # match info
+        do_std, dist, icdo_std,
+        # do info
+        doid, do_type, do_label
+    ) %>%
+    dplyr::arrange(icdo_id, icdo_type, dist, do_type, doid)
+
+
+readr::write_csv(tidy_df, match_res_file)
