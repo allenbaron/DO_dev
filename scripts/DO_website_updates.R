3c08d318781db3ca7a483d036a583f0c96e8c2df
J. Allen Baron <jabaron.phd@gmail.com>
2022-06-13 10:29:57 -0400

    Automate execution of DO_release_details

--- scripts/DO_website_updates.R
+++ scripts/DO_website_updates.R
@@ -5,17 +5,21 @@
 library(DO.utils)
 
 
-# UPDATE CSV FILES BEFORE RUNNING!!! --------------------------------------
+# MANUALLY Update Data!!! -------------------------------------------------
 
-# 1. data/citedby/DO_citedby.csv - EXECUTE scripts/citedby_full_procedure.R
-# 2. data/DO_release/DO_release_details.csv - EXECUTE scripts/DO_release_details.R
-# 3. data/DO_release/DO_term_def_counts.csv - EXECUTE
-#   scripts/DO_term_def_counts-from_git.py (in PyCharm).
+# 1. data/citedby/DO_citedby.csv - source(scripts/citedby_full_procedure.R)
+# 2. data/DO_release/DO_term_def_counts.csv - EXECUTE
+#   scripts/DO_term_def_counts-from_git.py (in PyCharm) --> convert to R script via DO.utils!!!
 #
 # Manually copy & paste stats from Google Sheet - DO_github_release_log
 #   (https://docs.google.com/spreadsheets/d/1-ZSUH43MJloR2EsBqHpGeY6IfKG7Gt8KBcU5remnoGI/edit#gid=269344614)
-# 4. data/DO_release/branch_counts.csv
-# 5. data/DO_release/cross_references.csv
+# 2. data/DO_release/branch_counts.csv
+# 3. data/DO_release/cross_references.csv
+
+
+# Automatically Update Data -----------------------------------------------
+
+source("scripts/DO_release_details.R") # data/DO_release/DO_release_details.csv
 
 
 # Generate Plots ----------------------------------------------------------
