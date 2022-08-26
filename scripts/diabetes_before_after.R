17f19f85c44547de4b989e74d1c86e4ef8ba2a91
J. Allen Baron <jabaron.phd@gmail.com>
2022-08-26 11:55:21 -0400

    Create earlier "before" tree for diabetes comparison
    Previous tree only included one change. This one includes
    changes from <2020 (added subtypes & actual rearrangements).
    
    RELEVANCE: Complex Disease Paper

--- scripts/diabetes_before_after.R
+++ scripts/diabetes_before_after.R
@@ -4,19 +4,27 @@
 #   csv for copy-paste into Illustrator as text tree.
 #
 # J. Allen Baron
-# 2022-03-24
+# Created: 2022-03-24
+# Last Updated: 2022-08-19
 
 
 # Specify releases & roots -------------------------------------------
 
 release <- list(
     before = "v2022-01-31",
-    after = "v2022-03-02"
+    before_v2 = "v2016-12-16",
+    after = "v2022-03-02",
+    after_v2 = "v2022-06-07",
+    after_v3 = "v2022-07-27"
 )
 
 root <- list(
-    before = "DOID:9351",
-    after = "DOID:0081062"
+    before = "DOID:9351", # same vor v2a tree
+    before2 = "DOID:0050524", # MODY
+    after = "DOID:0081062", # same for v2 & v3 trees
+    after2 = "DOID:1837", # diabetic ketoacidosis
+    after3 = "DOID:11716" # prediabetes syndrome
+    # diabetic peripheral angiopathy --> obsoleted, added manually
 )
 
 
@@ -53,3 +61,60 @@ after_tree <- DO.utils::format_subtree(after, root$after)
 gs <- "1Z5qH9AXHy_WbzYipRw5mIU7gBxx0ea8ZsSSuV_P67aA"
 googlesheets4::write_sheet(before_tree, gs, sheet = "before")
 googlesheets4::write_sheet(after_tree, gs, sheet = "after")
+
+
+# Create version 2 of "after" tree ----------------------------------------
+#   - minor update to "after" tree in v2022-06-07
+
+repo$capture_head()
+repo$checkout_tag(release$after_v2)
+
+after_v2 <- DO.utils::extract_subtree(repo$doid, root$after, reload = TRUE)
+after_v2_tree <- DO.utils::format_subtree(after_v2, root$after)
+googlesheets4::write_sheet(after_v2_tree, gs, sheet = "after_v2")
+
+repo$restore_head()
+
+# Create version 3 of "after" tree ----------------------------------------
+#   - update to "after" tree in v2022-07-27
+
+repo$capture_head()
+repo$checkout_tag(release$after_v3)
+
+after_v3 <- DO.utils::extract_subtree(repo$doid, root$after, reload = TRUE)
+after_v3_tree <- DO.utils::format_subtree(after_v3, root$after)
+googlesheets4::write_sheet(after_v3_tree, gs, sheet = "after_v3")
+
+repo$restore_head()
+
+
+# Create version 2 of "before" tree ----------------------------------------
+#   - update to "before" tree in v2016-12-06 (requires two trees)
+
+repo$capture_head()
+repo$checkout_tag(release$before_v2)
+
+before_v2a <- DO.utils::extract_subtree(repo$doid, root$before, reload = TRUE)
+before_v2a_tree <- DO.utils::format_subtree(before_v2a, root$before)
+
+# MODY, only disease + parent; no need to format tree (fails anyway)
+before_v2b <- DO.utils::extract_subtree(repo$doid, root$before2)
+
+googlesheets4::write_sheet(before_v2a_tree, gs, sheet = "before_v2a")
+googlesheets4::write_sheet(before_v2b, gs, sheet = "before_v2b")
+
+repo$restore_head()
+
+
+# Add "after_v3" diseases to match "before_v2" ----------------------------
+
+repo$capture_head()
+repo$checkout_tag(release$after_v3)
+
+after_v3b <- DO.utils::extract_subtree(repo$doid, root$after2, reload = TRUE)
+after_v3c <- DO.utils::extract_subtree(repo$doid, root$after3)
+
+googlesheets4::write_sheet(after_v3b, gs, sheet = "after_v3b")
+googlesheets4::write_sheet(after_v3c, gs, sheet = "after_v3c")
+
+repo$restore_head()
