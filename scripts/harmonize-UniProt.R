656224bc7e937a86299c048a33246c02d6a101d5
J. Allen Baron <jabaron.phd@gmail.com>
2023-06-12 10:57:53 -0400

    Update UniProt script to use UP xref IRIs
    Instead of provided input from UniProt that lacks xref prefixes
    (probably from a database dump of some kind), the script has been
    updated to use UniProt data from a SPARQL query.

--- scripts/harmonize-UniProt.R
+++ scripts/harmonize-UniProt.R
@@ -13,7 +13,7 @@ library(googlesheets4)
 de_path <- here::here("../Ontologies/HumanDiseaseOntology/src/ontology/doid-edit.owl")
 
 # uniprot xref data
-up_data_path <- here::here("data/mapping/uniprot-download_true_fields_id_2Cname_2Ckeywords_2Ccross_references-2023.05.23-13.38.57.88.tsv")
+up_data_path <- here::here("data/mapping/uniprot_disease-20230608.csv")
 
 # curation google sheet
 up_gs <- "https://docs.google.com/spreadsheets/d/1yUVActLsW_NDpMuocPN6J7AS9O2F4EyF-Ml70Je8DqY/edit#gid=0"
@@ -23,16 +23,31 @@ up_gs <- "https://docs.google.com/spreadsheets/d/1yUVActLsW_NDpMuocPN6J7AS9O2F4E
 # Load data ---------------------------------------------------------------
 
 # uniprot data
-up_data <- readr::read_tsv(up_data_path) %>%
-    janitor::clean_names()
+up_data <- readr::read_csv(up_data_path) %>%
+    dplyr::rename_with(.fn = stringr::str_squish) %>%
+    DO.utils::tidy_sparql() %>%
+    dplyr::mutate(
+        disease_entry_id = stringr::str_replace(
+            disease_iri,
+            "http://purl.uniprot.org/diseases/",
+            "up_disease:"
+        )
+    )
 
 up_tidy <- up_data %>%
-    dplyr::rename(
-        up_id = disease_entry_id,
-        up_label = name,
-        up_xref_lui = cross_reference
-    ) %>%
-    DO.utils::lengthen_col(up_xref_lui, delim = ",")
+    dplyr::rename(up_id = disease_entry_id, xref = xrefs) %>%
+    dplyr::rename_with(.fn = ~ paste0("up_", .x), .cols = -c(up_id, xref)) %>%
+    dplyr::select(up_id, up_label:up_keyword_iris) %>%
+    DO.utils::lengthen_col(xref) %>%
+    dplyr::mutate(
+        xref = stringr::str_replace_all(
+            xref,
+            c("http://id.nlm.nih.gov/mesh/" = "MESH:",
+              "http://purl.uniprot.org/mim/" = "OMIM:",
+              "http://purl.uniprot.org/medgen/" = "UMLS_CUI:"
+            )
+        )
+    )
 
 
 # DO data
@@ -45,23 +60,13 @@ do_data <- de_owl$query("sparql/for_mapping.rq") %>%
     DO.utils::tidy_sparql()
 
 do_tidy <- do_data %>%
-    # currently don't have prefixes to indicate namespaces for uniprot xrefs -->
-    # going to identify by perfect match to LUI only
-    #
-    # NOTE: UniProt appears to have only MIM, MedGen, and MeSH xrefs; if all of
-    # DO's xrefs are included, aberrant matches between UniProt xref LUIs will
-    # be made with DO xref LUIs from Orphanet and SnomedCT are generated
-    dplyr::filter(stringr::str_detect(xref, "MIM|MESH|UMLS")) %>%
-    dplyr::mutate(
-        xref_lui = stringr::str_remove(xref, ".*:"),
-        dplyr::across(
-            dplyr::contains("_type"),
-            DO.utils::to_curie
-        )
-    ) %>%
     DO.utils::collapse_col(.cols = xref_type, delim = ",") %>%
-    dplyr::rename_with(.cols = dplyr::everything(), .fn = ~ paste0("do_", .x))
-
+    dplyr::rename_with(.cols = -xref, .fn = ~ paste0("do_", .x)) %>%
+    tidyr::replace_na(list(do_dep = "FALSE")) %>%
+    dplyr::mutate(
+        do_dep = readr::parse_logical(do_dep),
+        dplyr::across(where(is.character), ~ dplyr::na_if(.x, "NA"))
+    )
 
 
 # COMPARISON!!! -----------------------------------------------------------
@@ -69,36 +74,42 @@ do_tidy <- do_data %>%
 # xref comparison only
 
 up_xref <- up_tidy %>%
-    dplyr::select(up_id, up_label, up_xref_lui) %>%
+    dplyr::select(up_id, up_label, xref) %>%
     dplyr::distinct()
 
 do_xref <- do_tidy %>%
-    dplyr::select(do_id:do_xref, do_xref_lui) %>%
+    dplyr::select(do_id:xref) %>%
     dplyr::distinct()
 
 xref_comp <- dplyr::full_join(
     up_xref,
     do_xref,
-    by = c("up_xref_lui" = "do_xref_lui"),
-    na_matches = "never",
+    by = "xref",
     relationship = "many-to-many"
 ) %>%
     dplyr::select(dplyr::contains("do_"), dplyr::everything()) %>%
     DO.utils::collapse_col(
-        .cols = c(up_xref_lui, do_xref, do_xref_type),
+        .cols = dplyr::one_of("xref", "do_xref_type"),
         na.rm = TRUE
     ) %>%
-    dplyr::arrange(do_id, up_id, up_xref_lui) %>%
-    # remove DO deprecated diseases that don't match UniProt
+    dplyr::arrange(do_id, up_id, xref) %>%
+    # remove DO deprecated diseases that don't match UniProt, retaining all DO & UP IDs
     dplyr::filter(
-        is.na(do_dep) | do_dep & !is.na(up_id)
+        !do_dep | do_dep & !is.na(up_id) | !is.na(up_id)
     ) %>%
     # identify UniProt diseases with NO match in DO
     dplyr::group_by(up_id) %>%
     dplyr::mutate(noDO = all(is.na(do_id))) %>%
     # identify DO diseases with NO match in UniProt
     dplyr::group_by(do_id) %>%
-    dplyr::mutate(noUP = all(is.na(up_id))) %>%
+    dplyr::mutate(
+        noUP = all(is.na(up_id)),
+        match_type = dplyr::if_else(
+            dplyr::n_distinct(up_id) == 1,
+            "single",
+            "multiple"
+        )
+    ) %>%
     dplyr::ungroup()
 
 # split out datasets for review
@@ -106,17 +117,20 @@ xref_out <- list(
     # UniProt diseases with no DO match
     not_in_DO = xref_comp %>%
         dplyr::filter(noDO) %>%
-        dplyr::select(dplyr::contains("up_")) %>%
+        dplyr::select(dplyr::contains("up_"), xref) %>%
         dplyr::rename_with(.fn = ~ stringr::str_remove(.x, "up_")),
     # DO diseases with no UniProt match
     not_in_UniProt = xref_comp %>%
         dplyr::filter(noUP) %>%
-        dplyr::select(dplyr::contains("do_")) %>%
+        dplyr::select(dplyr::contains("do_"), xref) %>%
         dplyr::rename_with(.fn = ~ stringr::str_remove(.x, "do_")) %>%
         dplyr::select(id, label, xref, xref_type),
     # diseases with xref matches
-    in_both = xref_comp %>%
-        dplyr::filter(!noDO, !noUP) %>%
+    in_both_single = xref_comp %>%
+        dplyr::filter(!noDO, !noUP, match_type == "single") %>%
+        dplyr::select(-dplyr::starts_with("no")),
+    in_both_multiple = xref_comp %>%
+        dplyr::filter(!noDO, !noUP, match_type != "single") %>%
         dplyr::select(-dplyr::starts_with("no"))
 )
 
