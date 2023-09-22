172c274bd51a278ec0cc580269163f803ed87586
J. Allen Baron <jabaron.phd@gmail.com>
2023-09-22 10:24:27 -0400

    Update xref_to_SSSOM to match SSSOM spec

--- scripts/xref_skos_to_SSSOM.R
+++ scripts/xref_skos_to_SSSOM.R
@@ -6,8 +6,8 @@ library(DO.utils)
 library(googlesheets4)
 
 de_path <- here::here("../Ontologies/HumanDiseaseOntology/src/ontology/doid-edit.owl")
-gs <- "1cKEXlbdrNIT5KgML3H5lRPdgx4p5Lc1yWGRuKTmViZg" # curated_DOID_mappings Google Sheet
-gs_sheet <- "all_mappings"
+gs <- "1qAzDm9_jFe_a0gqxDpWI9ik8FeSdI-aYr7smbGnSNpk" # curated_DOID_mappings Google Sheet
+gs_sheet <- "in_DOID"
 
 tmp_file <- tempfile(fileext = ".csv")
 
@@ -49,21 +49,50 @@ mappings_keep <- mappings %>%
     # label problematic mappings (xref + skos:(broad/narrow)Match for review
     #   --> identified as simple duplicates (shouldn't be any)
     dplyr::mutate(
-        subject_id = DO.utils::build_hyperlink(
-        needs_review = DO.utils::all_duplicated(paste0(subject_id, object_id))
+        mapping_justification = dplyr::if_else(
+            stringr::str_detect(object_id, "ORDO|OMIM|GARD|EFO|KEGG|ICDO|MEDDRA"),
+            "semapv:ManualMappingCuration",
+            NA_character_
+        ),
+        other = paste0(
+            "needs_review: ",
+            DO.utils::all_duplicated(paste0(subject_id, object_id)),
+            ",\nreview_date: '',\n",
+            "subject_obsoleted_version: '',\n",
+            "object_obsoleted_version: ''"
         )
     ) %>%
     # add curation columns and re-arrange
     DO.utils::append_empty_col(
-        c("subject_id", "subject_label", "predicate_id", "object_id",
-          "object_label", "mapping_justification", "contributor", "date",
-          "needs_review", "notes", "review_doc"),
+        c("subject_id", "subject_label", "predicate_id", "predicate_modifier",
+          "object_id", "object_label", "mapping_justification", "confidence",
+          "comment", "author_id", "mapping_date", "reviewer_id",
+          "issue_tracker", "see_also", "curation_rule", "other"
+        ),
         order = TRUE
     )
 
-# save to
-googlesheets4::write_sheet(mappings_keep, ss = gs, sheet = gs_sheet)
+# save to Google Sheet with links for quicker review
+mappings_gs <- mappings_keep %>%
+    dplyr::mutate(
+        subject_id = DO.utils::build_hyperlink(
+            x = stringr::str_remove(subject_id, ".*:"),
+            url = "DOID",
+            txt = subject_id,
+            as = "gs"
+        ),
+        object_id = DO.utils::build_hyperlink(
+            x = stringr::str_remove(object_id, ".*:"),
+            url = stringr::str_remove(object_id, "(_[0-9]{4}_[0-9]{2}_[0-9]{2})?:.*"),
+            txt = object_id,
+            as = "gs"
+        )
+    )
 
+googlesheets4::write_sheet(mappings_gs, ss = gs, sheet = gs_sheet)
+
+
+# capture redundant removed mappings
 mappings_rm <- mappings %>%
     dplyr::anti_join(
         mappings_keep,
@@ -75,7 +104,43 @@ mappings_rm <- mappings %>%
         by = c("subject_id", "object_id")
     )
 
-# redundant mappings stats (only removed for manual curation... added back
+# redundant mappings stats (only removed for manual curation... plan to add back
 # automatically during build
 dplyr::count(mappings_rm, predicate_id, replaced_by)
 
+# "slots" in SSSOM spec
+sssom <- c(
+    "author_id", "author_label", "comment", "confidence", "creator_id",
+    "creator_label", "curation_rule", "curation_rule_text", "documentation",
+    "homepage", "imports", "issue_tracker", "issue_tracker_item",
+    "last_updated", "license", "literal", "literal_datatype",
+    "literal_preprocessing", "literal_source", "literal_source_version",
+    "local_name", "mapping_cardinality", "mapping_date",
+    "mapping_justification", "mapping_provider", "mapping_registry_description",
+    "mapping_registry_id", "mapping_registry_title", "mapping_set_description",
+    "mapping_set_group", "mapping_set_id", "mapping_set_references",
+    "mapping_set_source", "mapping_set_title", "mapping_set_version",
+    "mapping_source", "mapping_tool", "mapping_tool_version", "mappings",
+    "match_string", "mirror_from", "object_category", "object_id",
+    "object_label", "object_match_field", "object_preprocessing",
+    "object_source", "object_source_version", "object_type", "other",
+    "predicate_id", "predicate_label", "predicate_modifier", "predicate_type",
+    "publication_date", "registry_confidence", "reviewer_id", "reviewer_label",
+    "see_also", "semantic_similarity_measure", "semantic_similarity_score",
+    "similarity_measure", "similarity_score", "subject_category", "subject_id",
+    "subject_label", "subject_match_field", "subject_preprocessing",
+    "subject_source", "subject_source_version", "subject_type"
+)
+
+# slots desired for curation but NOT in SSSOM spec; NOTE could be in spec for
+# SSSOM if formatted as key-value list in "other" column
+# handle these at build time by:
+#   1. Dropping any rows with values in .subject_obsoleted_version (primarily
+#   retained for historical purposes)
+#   2. Keeping rows with .object_obsoleted_version and annotating axiom with
+#   something to indicate deprecation status; alternatively could append to
+#   term comment in programmatically accessible way -> "obsoleted_xrefs: <xref1>, ..."
+not_sssom <- c(
+    ".review_date", ".needs_review", ".subject_obsoleted_version",
+    ".object_obsoleted_version"
+)
