f0b9910a42481f5874f5781937a08e6aee3f2c71
J. Allen Baron <jabaron.phd@gmail.com>
2025-04-02 18:41:02 -0400

    Improve website translation scripts
    Now automate translation better & can fix changes caused only by
    whitespace.

--- scripts/spanish-check_and_replace_translation.R
+++ scripts/spanish-check_and_replace_translation.R
@@ -1,67 +1,66 @@
 # compare previous messages.po extraction with current doc
 library(tidyverse)
 library(googlesheets4)
+library(DO.utils)
 
-# load & parse messages.po
-sf <- "~/Documents/DO_website/do_trunk/translations/es/LC_MESSAGES/messages.po"
-sraw <- readr::read_file(sf)
-sparse <- stringr::str_match_all(
-    sraw,
-    stringr::regex(
-        "\n(?<loc>#: .+?)\nmsgid (?<english>.+?)\nmsgstr (?<spanish>.+?)(?=\n\n#:)",
-        dotall = TRUE
-    )
-)[[1]][, 2:4] |>
-    tibble::as_tibble() |>
-    dplyr::mutate(
-        fuzzy = stringr::str_detect(loc, stringr::coll("#, fuzzy")),
-        loc = stringr::str_remove(loc, "\n#, fuzzy")
-    )
+# Spanish messages.po file
+es_path <- "~/Documents/DO_website/do_trunk/translations/es/LC_MESSAGES/messages.po"
+
+# Google sheet for translation
+ss <- "https://docs.google.com/spreadsheets/d/1c5ih1CX11EL47B0dGJB-Y9jnlMVw2agQ3kOtnZmJCP8/"
+sheet <- "Sheet1"
+
+
+# Custom function ---------------------------------------------------------
+
+# Read & parse function for messages.po files
+read_parse_msgpo <- function(path) {
+    .raw <- readr::read_file(path)
+    .parse <- stringr::str_match_all(
+        .raw,
+        stringr::regex(
+            "\n(?<loc>#: .+?)\nmsgid (?<english>.+?)\nmsgstr (?<spanish>.+?)(?=\n\n#:)",
+            dotall = TRUE
+        )
+    )[[1]] |>
+        tibble::as_tibble(.name_repair = ~ c("match", "loc", "english", "spanish")) |>
+        dplyr::mutate(
+            fuzzy = stringr::str_detect(loc, stringr::coll("#, fuzzy")),
+            loc = stringr::str_remove(loc, "\n#, fuzzy")
+        )
+    list(raw = .raw, parsed = .parse)
+}
+
+
+
+# Processing --------------------------------------------------------------
+
+es <- read_parse_msgpo(es_path)
 
 # Load translation google sheet (assume created today)
-sheet_nm <- paste0("translation-", format(Sys.Date(), "%Y%m%d"))
-sf_gs <- googlesheets4::read_sheet(
-    ss = "1DTKwKr6AYSiLsTmnKavtKN6b52xJM0Kp3LTh1bxxknc",
-    sheet = sheet_nm
-)
+es_gt <- googlesheets4::read_sheet(ss, sheet) |>
+    dplyr::mutate(spanish = DO.utils::sandwich_text(.data$spanish, '"'))
 
 
-# Compare messages.po with google sheet -----------------------------------
+# Compare messages.po with translation ------------------------------------
 
 # rows in sheet that don't match messages.po
-missing <- dplyr::anti_join(sf_gs, sparse, by = c("loc", "fuzzy", "english"))
-dplyr::count(missing, sort = TRUE)
-
-# remaining rows that do match messages.po
-remain <- dplyr::semi_join(sf_gs, sparse, by = c("loc", "fuzzy", "english"))
-
-# check those that state a check is needed in Notes column (likely fix after spanish replacement)
-check <- remain |>
-    # dplyr::filter(str_detect(Notes, "check in")) |>
-    dplyr::mutate(
-        # identify multi-line in messages.po which may not correspond directly
-        # to text in templates... these will need to be checked manually
-        multi = stringr::str_detect(english, '^""'),
-        # convert template file paths to character vectors
-        templates = stringr::str_remove(loc, "^#: ") |>
-            stringr::str_remove_all(":[0-9]+") |>
-            stringr::str_split("(\n)?(#:)? ")
-    )
+missing <- dplyr::anti_join(es_gt, es$parsed, by = c("loc", "fuzzy", "english"))
+if (nrow(missing) > 0) {
+    dplyr::count(missing, sort = TRUE)
+    stop("Data mismatch!")
+}
 
 # identify places where spanish is new (i.e. messages.po = "") or differs
-sp_diff_all <- sf_gs |>
+es_diff <- es_gt |>
     dplyr::anti_join(missing) |>
     dplyr::anti_join(
-        sparse,
+        es$parsed,
         by = c("loc", "fuzzy", "english", "spanish")
     ) |>
     dplyr::left_join(
-        dplyr::select(sparse, loc, fuzzy, english, sp_current = spanish)
+        dplyr::select(es$parsed, "loc", "fuzzy", "english", es_current = "spanish")
     )
-
-# new only
-sp_diff <- sp_diff_all |>
-    dplyr::filter(sp_current == '""' | is.na(sp_current))
 
 
 # Replace translation with update from Google Sheet -----------------------
@@ -70,104 +69,100 @@ sp_diff <- sp_diff_all |>
 #   different existing translation already in messages.po
 
 # build replacement dictionary
-match_replace_df <- sp_diff_all |>
-    dplyr::rowwise() |>
+es_replace <- es_diff  |>
     dplyr::mutate(
-        match = paste0(
-            "\n", loc,
-            "\nmsgid ", english,
-            "\nmsgstr ", sp_current
-        ),
+        match = stringr::str_escape(.data$match),
         replace = paste0(
-            "\n", loc,
+            loc,
             "\nmsgid ", english,
             "\nmsgstr ", spanish
-        )
-    )
-match_replace <- purrr::set_names(
-    match_replace_df$replace,
-    match_replace_df$match
-)
+        ) |>
+            stringr::str_escape()
+    ) |>
+    (function(.df) { purrr::set_names(.df$replace, .df$match) })()
 
 # replace in raw read of messages.po
-sraw_new <- stringr::str_replace_all(
-    sraw,
-    match_replace
+es_raw_new <- stringr::str_replace_all(
+    es$raw,
+    es_replace
 )
 
-readr::write_file(sraw_new, sf)
+readr::write_file(es_raw_new, es_path)
 
+
+#################### NO GUARANTEE CODE BELOW STILL WORKS ####################
 
 # For comparison after updating Spanish (ignore location differences) --------
 #   ignore differences in file location (many numbers have changed), fuzzy
 #   (removed from file) and differences in lines due to line breaks
 # rows in sheet that don't match messages.po
-sf_gs_adjust <- dplyr::mutate(
-    sf_gs,
-    english_orig = english,
-    english = str_replace_all(english, c('"|\n' = "", " +" = " "))
-)
 
-sparse_adjust <- dplyr::mutate(
-    sparse,
-    english_orig = english,
-    english = str_replace_all(english, c('"|\n' = "", " +" = " "))
-)
-
-missing <- dplyr::anti_join(sf_gs_adjust, sparse_adjust, by = "english")
-dplyr::count(missing, Notes, sort = TRUE)
-# review rows in z data.frame (excluded ones are expected not to match for reason in Notes column)
-z <- dplyr::filter(missing, !stringr::str_detect(Notes, "NO|ignored|combined") | is.na(Notes))
-
-sp_diff_all <- sf_gs_adjust |>
-    dplyr::anti_join(missing) |>
-    dplyr::anti_join(
-        sparse_adjust,
-        by = c("english", "spanish")
-    ) |>
-    dplyr::left_join(
-        dplyr::select(sparse_adjust, english, english_orig, sp_current = spanish)
-    ) |>
-    dplyr::relocate(Notes, .after = dplyr::last_col())
-
-# build replacement dictionary
-match_replace_df <- sp_diff_all |>
-    dplyr::rowwise() |>
-    dplyr::mutate(
-        match = paste0(
-            "\nmsgid ", english_orig,
-            "\nmsgstr ", sp_current
-        ),
-        replace = paste0(
-            "\nmsgid ", english_orig,
-            "\nmsgstr ", spanish
-        )
-    )
-match_replace <- purrr::set_names(
-    match_replace_df$replace,
-    match_replace_df$match
-)
-
-# replace in raw read of messages.po
-sraw_new <- stringr::str_replace_all(
-    sraw,
-    match_replace
-)
-
-readr::write_file(sraw_new, sf)
-
-
-## didn't replace everything... check remainder manually
-
-z1 <- sp_diff_all |>
-    dplyr::mutate(
-        sp_current = str_replace_all(sp_current, c('"|\n' = "", " +" = " ")),
-        spanish = str_replace_all(spanish, c('"|\n' = "", " +" = " "))
-    ) |>
-    dplyr::filter(sp_current != spanish) |>
-    dplyr::select(english, sp_current, spanish)
-
-googlesheets4::write_sheet(z1, "https://docs.google.com/spreadsheets/d/1DTKwKr6AYSiLsTmnKavtKN6b52xJM0Kp3LTh1bxxknc/edit?gid=1805918238#gid=1805918238", "temp-fix")
+# es_gt_adjust <- dplyr::mutate(
+#     es_gt,
+#     english_orig = english,
+#     english = str_replace_all(english, c('"|\n' = "", " +" = " "))
+# )
+#
+# sparse_adjust <- dplyr::mutate(
+#     es$parsed,
+#     english_orig = english,
+#     english = str_replace_all(english, c('"|\n' = "", " +" = " "))
+# )
+#
+# missing <- dplyr::anti_join(es_gt_adjust, sparse_adjust, by = "english")
+# dplyr::count(missing, Notes, sort = TRUE)
+# # review rows in z data.frame (excluded ones are expected not to match for reason in Notes column)
+# z <- dplyr::filter(missing, !stringr::str_detect(Notes, "NO|ignored|combined") | is.na(Notes))
+#
+# sp_diff_all <- es_gt_adjust |>
+#     dplyr::anti_join(missing) |>
+#     dplyr::anti_join(
+#         sparse_adjust,
+#         by = c("english", "spanish")
+#     ) |>
+#     dplyr::left_join(
+#         dplyr::select(sparse_adjust, english, english_orig, es_current = spanish)
+#     ) |>
+#     dplyr::relocate(Notes, .after = dplyr::last_col())
+#
+# # build replacement dictionary
+# es_replace_df <- sp_diff_all |>
+#     dplyr::rowwise() |>
+#     dplyr::mutate(
+#         match = paste0(
+#             "\nmsgid ", english_orig,
+#             "\nmsgstr ", es_current
+#         ),
+#         replace = paste0(
+#             "\nmsgid ", english_orig,
+#             "\nmsgstr ", spanish
+#         )
+#     )
+# es_replace <- purrr::set_names(
+#     es_replace_df$replace,
+#     es_replace_df$match
+# )
+#
+# # replace in raw read of messages.po
+# es_raw_new <- stringr::str_replace_all(
+#     es$raw,
+#     es_replace
+# )
+#
+# readr::write_file(es_raw_new, es_path)
+#
+#
+# ## didn't replace everything... check remainder manually
+#
+# z1 <- sp_diff_all |>
+#     dplyr::mutate(
+#         es_current = str_replace_all(es_current, c('"|\n' = "", " +" = " ")),
+#         spanish = str_replace_all(spanish, c('"|\n' = "", " +" = " "))
+#     ) |>
+#     dplyr::filter(es_current != spanish) |>
+#     dplyr::select(english, es_current, spanish)
+#
+# googlesheets4::write_sheet(z1, "https://docs.google.com/spreadsheets/d/1DTKwKr6AYSiLsTmnKavtKN6b52xJM0Kp3LTh1bxxknc/edit?gid=1805918238#gid=1805918238", "temp-fix")
 
 # For manual check... ignoring now that it's completed --------------------
 
--- scripts/spanish-parse_msg_po.R
+++ scripts/spanish-parse_msg_po.R
@@ -2,27 +2,131 @@
 # to translation
 library(tidyverse)
 library(googlesheets4)
+library(DO.utils)
+library(googledrive)
 
-sf <- "~/Documents/DO_website/do_trunk/translations/es/LC_MESSAGES/messages.po"
-sraw <- readr::read_file(sf)
-sparse <- stringr::str_match_all(
-    sraw,
+# Spanish messages.po file
+es_path <- "~/Documents/DO_website/do_trunk/translations/es/LC_MESSAGES/messages.po"
+
+# Google sheet for translation
+ss <- "https://docs.google.com/spreadsheets/d/1c5ih1CX11EL47B0dGJB-Y9jnlMVw2agQ3kOtnZmJCP8/"
+sheet <- "Sheet1"
+
+
+# Custom function ---------------------------------------------------------
+
+# Read & parse function for messages.po files
+read_parse_msgpo <- function(path) {
+    .raw <- readr::read_file(path)
+    .parse <- stringr::str_match_all(
+        .raw,
+        stringr::regex(
+            "\n(?<loc>#: .+?)\nmsgid (?<english>.+?)\nmsgstr (?<spanish>.+?)(?=\n\n#:)",
+            dotall = TRUE
+        )
+    )[[1]] |>
+        tibble::as_tibble(.name_repair = ~ c("match", "loc", "english", "spanish")) |>
+        dplyr::mutate(
+            fuzzy = stringr::str_detect(loc, stringr::coll("#, fuzzy")),
+            loc = stringr::str_remove(loc, "\n#, fuzzy")
+        )
+    list(raw = .raw, parsed = .parse)
+}
+
+
+
+# Processing --------------------------------------------------------------
+
+es <- read_parse_msgpo(es_path)
+
+es_parsed_prior <- stringr::str_match_all(
+    es$raw,
     stringr::regex(
-        "\n(?<loc>#: .+?)\nmsgid (?<english>.+?)\nmsgstr (?<spanish>.+?)(?=\n\n#:)",
+        "\n#~ msgid (?<english>.+?)\n#~ msgstr (?<spanish>.+?)(?=\n\n#~)",
         dotall = TRUE
     )
-)[[1]][, 2:4] %>%
-    tibble::as_tibble() %>%
+)[[1]] |>
+    tibble::as_tibble(.name_repair = ~ c("match_rm", "english", "spanish")) |>
     dplyr::mutate(
-        fuzzy = stringr::str_detect(loc, stringr::coll("#, fuzzy")),
-        loc = stringr::str_remove(loc, "\n#, fuzzy")
+        dplyr::across(
+            .cols = c("english", "spanish"),
+            .fns = ~ stringr::str_remove_all(.x, "#~ ")
+        ),
+        english_match = stringr::str_replace_all(
+            .data$english,
+            "\\\\n|\n|\"",
+            " "
+        ) |>
+            stringr::str_squish()
+    ) |>
+    dplyr::filter(.data$spanish != '""') |>
+    dplyr::select("english_match", spanish_fix = "spanish", "match_rm")
+
+# restore Spanish that only changed due to whitespace
+es_fix <- es$parsed |>
+    dplyr::mutate(
+        english_match = stringr::str_replace_all(
+            .data$english,
+            "\\\\n|\n|\"",
+            " "
+        ) |>
+            stringr::str_squish()
+    ) |>
+    dplyr::right_join(es_parsed_prior, by = "english_match") |>
+    dplyr::filter(!is.na(.data$match))
+
+if (nrow(es_fix) > 0) {
+    message("Restoring Spanish differing only by whitespace")
+    es_replace <- es_fix |>
+        dplyr::mutate(
+            match_rm = paste0("\n", .data$match_rm),
+            dplyr::across(dplyr::starts_with("match"), stringr::str_escape),
+            replace = paste0(
+                "\n", .data$loc,
+                "\nmsgid ", .data$english,
+                "\nmsgstr ", .data$spanish_fix
+            ) |>
+                stringr::str_escape()
+        ) |>
+        (function(.df) {
+            purrr::set_names(
+                c(.df$replace, rep("", nrow(.df))),
+                c(.df$match, .df$match_rm)
+            )
+        })()
+
+    # replace in raw read of messages.po
+    es_raw_new <- stringr::str_replace_all(
+        es$raw,
+        es_replace
     )
 
-if (!interactive()) {
-    sheet_nm <- paste0("translation-", format(Sys.Date(), "%Y%m%d"))
-    googlesheets4::write_sheet(
-        sparse,
-        ss = "1DTKwKr6AYSiLsTmnKavtKN6b52xJM0Kp3LTh1bxxknc",
-        sheet = sheet_nm
+    readr::write_file(es_raw_new, es_path)
+
+    es <- read_parse_msgpo(es_path)
+}
+
+
+if (any(es$parsed$fuzzy)) {
+    stop("Fuzzy matches exist!!!")
+} else {
+    es_gt <- es$parsed |>
+        dplyr::filter(.data$spanish == '""') |>
+        dplyr::anti_join(es_fix, by = "match") |>
+        dplyr::mutate(
+            spanish = stringr::str_replace_all(english, "\\\\n|\n|\"", " ") |>
+                stringr::str_squish() |>
+                DO.utils::sandwich_text('"') |>
+                (\(x) paste0("=GOOGLETRANSLATE(", x, ", \"en\", \"es\")"))() |>
+                googlesheets4::gs4_formula()
+        )
+
+    gs <- googlesheets4::gs4_get(ss)
+    message(
+        paste0(
+            "Submitting ", nrow(es_gt), " units to ", gs$name, " for translation"
+        )
     )
+
+    googlesheets4::write_sheet(es_gt, ss = ss, sheet = sheet)
 }
